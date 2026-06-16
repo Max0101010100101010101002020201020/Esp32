84af68373569a75c42b56216bd56035d1be3db92
Max0101010100101010101002020201020 <dilanlopez294ka@gmail.com>
2026-06-16 14:18:36 -0700

    Corregir base64 decode para JWT

--- src/base64.cpp
+++ src/base64.cpp
@@ -1,5 +1,6 @@
 #include "utils.h"
 const char _b64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 String base64::encode(const String& data) {
   String out;
   int val = 0, valb = -6;
@@ -15,7 +16,21 @@ String base64::encode(const String& data) {
   while (out.length() % 4) out += '=';
   return out;
 }
+
 String base64::decode(const String& data) {
-  // implementación mínima omitida para brevedad, no se usa en el flujo crítico
-  return "";
+  String out;
+  std::vector<int> T(256, -1);
+  for (int i = 0; i < 64; i++) T[_b64[i]] = i;
+  int val = 0, valb = -8;
+  for (unsigned char c : data) {
+    if (c == '=') break;
+    if (T[c] == -1) continue;
+    val = (val << 6) + T[c];
+    valb += 6;
+    if (valb >= 0) {
+      out += char((val >> valb) & 0xFF);
+      valb -= 8;
+    }
+  }
+  return out;
 }
